b3acc2099882919db58de43c9cad39d9c67d758c
stweedo <108593831+stweedo@users.noreply.github.com>
2024-04-11 18:29:48 -0500

    doubletap timer to 200ms

--- apps/boxclk/app.js
+++ apps/boxclk/app.js
@@ -366,7 +366,7 @@
       }
       doubleTapTimer = setTimeout(() => {
         doubleTapTimer = null;
-      }, 250); // Increase or decrease this value based on the desired double tap timing
+      }, 200); // Increase or decrease this value based on the desired double tap timing
     };
 
     // ------------------------------------
